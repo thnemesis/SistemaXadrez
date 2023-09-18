4403de4709b6ec24c5c102445d3283c941cf1321
Thiago Silva <thnemesis@outlook.com>
2023-09-18 09:01:23 -0300

    tabuleiro impresso no git

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,34 @@
 package application;
 
-import boardgame.Position;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
 		
-		System.out.println(pos);
+		while(true) {
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
+		
 	}
 
 }
